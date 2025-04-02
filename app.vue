c68f113939a9e02b9cd7f8b7a77cdf58b4c35087
Miha Gradišar <gradisar.mih4@gmail.com>
2025-04-02 13:37:45 +0200

    Project Setup

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+  // Component imports
+  import AppHeader from "./components/AppHeader.vue"
+  import AppCharacter from "./components/AppCharacter.vue"
+  import AppEditButton from "./components/AppEditButton.vue"
+  import AppApplyButton from "./components/AppApplyButton.vue"
+  import AppCancelButton from "./components/AppCancelButton.vue"
+
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  
 </template>
